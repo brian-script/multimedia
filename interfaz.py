cc6798d4c53f626e6ba8d613b876d98e162b2371
brian <brianarguello358@gmail.com>
2024-10-05 16:18:37 -0400

    agregando funciones

--- interfaz.py
+++ interfaz.py
@@ -1,10 +1,11 @@
-import yt_dlp
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+import logica
 
 class Interfaz(App):
     def build(self):
@@ -18,16 +19,21 @@ class Interfaz(App):
         body.center_y = float_body.center_y
 
         titulo = Label(text = "No Name")        
-        campo_texto = TextInput(multiline = False, size_hint = (0.8, None), height = 40)
+        self.campo_texto = TextInput(multiline = False, height = 40)
+        self.campo_texto.bind(on_text_validate = self.enviardatos)
+        enviar = Button(text = "Enviar", background_color = "red")
 
         body.add_widget(titulo)
-        body.add_widget(campo_texto)
+        body.add_widget(self.campo_texto)
+        body.add_widget(enviar)
         
         anchor_body.add_widget(body)
         float_body.add_widget(anchor_body)
         return float_body
 
-
+    def enviardatos(self, instance):
+        link = self.campo_texto.text
+        logica.download_mp4(link)
 
 if __name__ == "__main__":
     Interfaz().run()
---
+++ logica.py
@@ -0,0 +1,27 @@
+import os
+import yt_dlp
+
+directorio = "/home/bsap-vga/Downloads"
+
+def download_mp3(link):
+    yt_dlps = {
+            "format": "bestaudio/best",
+            "postprocessors": [{
+                "key": "FFmpegExtractAudio",
+                "preferredcodec": "mp4",
+                "preferredquality": "192"
+                }],
+            "outtmpl": os.path.join(directorio, "%(title)s.%(ext)s"),
+            }
+
+    with yt_dlp.YoutubeDL(yt_dlps) as ytl:
+        ytl.download([link])
+
+def download_mp4(link):
+    yt_dlps = {
+            "format": "bestvideo+bestaudio/best",
+            "outtmpl": os.path.join(directorio, "%(title)s.%(ext)s")
+            }
+
+    with yt_dlp.YoutubeDL(yt_dlps) as ytl:
+        ytl.download([link])
